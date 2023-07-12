f29369076b1b5a7221e1074f95c164801cdf01ff
Aswin Vignesh <workspace703@gmail.com>
2023-07-12 10:04:46 +0530

    responsiveness bug fix add and remove range slider

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const navItems = document.querySelector(".nav-items");
 hamburger.onclick = function (e) {
   navItems.classList.toggle("active");
 }
+// to close hamburger on the click
 navItem.forEach((navs) => {
   navs.onclick = function (e) {
     // if(navItems.classList.contains('active'))
@@ -273,43 +274,3 @@ for (let i = 0; i < contentList.length; i++) {
 }
 
 
-// Range Slider
-
-// on fee per hour input
-$('#input-fee-per-hour').on('input', function(){
-  // update total fee text
-  $('#text-fee-per-hour').text(formatNumber(calculateTotalFee()));
-});
-
-// on total hours input
-$('#input-total-hours').on('input', function(){
-  // update total fee text
-  $('#text-fee-per-hour').text(formatNumber(calculateTotalFee()));
-});
-
-// on total projects input
-$('#input-total-projects').on('input', function(){
-  // update project count text
-  $('.hack50-project-count').text($(this).val());
-  // update total fee text
-  $('#text-fee-per-hour').text(formatNumber(calculateTotalFee()));
-});
-
-// calculate total fee
-function calculateTotalFee(){
-  // get fee per hour value
-  const feePerHr = Number($('#input-fee-per-hour').val());
-  // get total hours
-  const totalHrs = Number($('#input-total-hours').val());
-  // get total number of projects
-  const totalProjects = Number($('#input-total-projects').val());
-  
-  // multiply feePerHr by totalHrs by totalProjects to get total fee
-  return feePerHr * totalHrs * totalProjects;
-}
-
-// format number function
-// e.g. 3500 becomes 3,500
-function formatNumber(num){
-  return new Intl.NumberFormat().format(num);
-}
